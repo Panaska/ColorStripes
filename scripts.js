4b1e8dc5cc3a11e0e3c80d3b3cb197946876235b
Elena Zelenskaya <boojum1@yandex.ru>
2019-06-27 21:58:06 -0700

    Adding stars, hearts and more!

--- scripts.js
+++ scripts.js
@@ -1,40 +1,99 @@
 "use strict";
 
-
 function randomColor() {
+  let colorAuto = "#";
 
-    var nodelist = document.getElementsByClassName("differentColors").length;
+  for (let f = 0; f < 6; f++) {
+      let rand = Math.floor(Math.random() * 10);
+      if (rand > 5 && f == 0) {
+          rand = "B";
+      } else if (rand > 2 && f == 3) {
+          rand = "E";
+      } else if (rand > 7 && f < 2) {
+          rand = "A";
+      } else if (rand > 8 && f < 5) {
+          rand = "C";
+      } else if (rand > 3 && f < 1) {
+          rand = "D";
+      } else if (rand == 4 && f < 7) {
+          rand = "F";
+      }
+      colorAuto += rand;
+  }
+
+return colorAuto;
+}
+
+
+function paintStripes() {
+
+    let nodelist = document.getElementsByClassName("differentColors").length;
     //console.log(nodelist);
-    for (var i = 1; i <= nodelist; i++) {
-        var k = "wrap" + i;
-        var node = document.getElementById(k);
-			//	console.log(k);
-        var colorAuto = "#";
+    for (let i = 1; i <= nodelist; i++) {
+        let k = "wrap" + i;
+        let node = document.getElementsByClassName(k)[0];
 
-        for (var f = 0; f < 6; f++) {
-            var rand = Math.floor(Math.random() * 10);
-            if (rand > 5 && f == 0) {
-                rand = "B";
-            } else if (rand > 2 && f == 3) {
-                rand = "E";
-            } else if (rand > 7 && f < 2) {
-                rand = "A";
-            } else if (rand > 8 && f < 5) {
-                rand = "C";
-            } else if (rand > 3 && f < 1) {
-                rand = "D";
-            } else if (rand == 4 && f < 7) {
-                rand = "F";
-            }
-            colorAuto += rand;
-        }
-      //  console.log(colorAuto);
-//console.log(node);
-        node.style.backgroundColor = colorAuto;
+        node.style.backgroundColor = randomColor();
 
-       console.log(node.style.backgroundColor);
+      // console.log(node.style.backgroundColor);
 
       //  node.style.boxShadow = '2px 2px 5px 1px ' + colorAuto + ',  4px 4px 2px 0px black';
 
     }
+}
+
+function addingSquare() {
+  let nodelist = document.getElementsByClassName("square").length;
+  for (let i = 1; i <= nodelist; i++) {
+    let k = "square" + i;
+    let node = document.getElementById(k);
+    let randTop = Math.floor(Math.random() * 85);
+    let randLeft = Math.floor(Math.random() * 85);
+    node.style.top = randTop + "%";
+    node.style.left = randLeft + "%";
+node.style.visibility="visible";
+node.style.backgroundColor=randomColor();
+  }
+}
+
+function addingCircle() {
+  let nodelist = document.getElementsByClassName("round").length;
+  for (let i = 1; i <= nodelist; i++) {
+    let k = "round" + i;
+    let node = document.getElementById(k);
+    let randTop = Math.floor(Math.random() * 85);
+    let randLeft = Math.floor(Math.random() * 85);
+    node.style.top = randTop + "%";
+    node.style.left = randLeft + "%";
+node.style.visibility="visible";
+node.style.backgroundColor=randomColor();
+  }
+}
+
+function addingHeart() {
+  let nodelist = document.getElementsByClassName("heart").length;
+  for (let i = 1; i <= nodelist; i++) {
+    let k = "heart" + i;
+    let node = document.getElementById(k);
+    let randTop = Math.floor(Math.random() * 83);
+    let randLeft = Math.floor(Math.random() * 83);
+    node.style.top = randTop + "%";
+    node.style.left = randLeft + "%";
+node.style.visibility="visible";
+node.style.backgroundColor=randomColor();
+  }
+}
+
+function addingStar() {
+  let nodelist = document.getElementsByClassName("star").length;
+  for (let i = 1; i <= nodelist; i++) {
+    let k = "star" + i;
+    let node = document.getElementById(k);
+    let randTop = Math.floor(Math.random() * 81);
+    let randLeft = Math.floor(Math.random() * 81);
+    node.style.top = randTop + "%";
+    node.style.left = randLeft + "%";
+node.style.visibility="visible";
+node.style.borderBottomColor=randomColor();
+  }
 }
